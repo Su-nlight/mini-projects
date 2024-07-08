618ef18a97ee965f800957b8227bd61b03ea332f
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2024-07-08 22:10:42 +0530

    Update docker_setup.sh

--- ntfy-trigger/docker_setup.sh
+++ ntfy-trigger/docker_setup.sh
@@ -12,16 +12,17 @@ cd /etc/ntfy
 wget -O server.yml https://raw.githubusercontent.com/Su-nlight/mini-projects/main/ntfy-trigger/server.yml
 
 
-sudo sed -i "14s/.*/base-url: http:\/\/$(hostname -I | awk '{print $1}')/" server.yml
+sed -i "14s/.*/base-url: http:\/\/$(hostname -I | awk '{print $1}')/" server.yml
+# sed -i "226s/.*/upstream-base-url: \"https://ntfy.sh\" " server.yml
 
 cd ~
 
-apt install docker.io
+# change port number set by me as 2500 to your desired port.
 
 sudo docker run \
 -v /var/cache/ntfy:/var/cache/ntfy \
 -v /etc/ntfy:/etc/ntfy \
--p 80:2500 \
+-p 2500:80 \
 -itd \
 binwiederhier/ntfy \
 serve \
